9f4642cd32493a4ab128454a383eeaa4d796de88
Gintarich <gintars.briedis@gmail.com>
2024-02-03 22:59:45 +0200

    Starting to work on debbugging

--- lua/GB/packer.lua
+++ lua/GB/packer.lua
@@ -27,7 +27,7 @@ return require('packer').startup(function(use)
 	use( 'ThePrimeagen/harpoon' )
 	use('mbbill/undotree')
 	use('tpope/vim-fugitive')
-    use('ThePrimeagen/vim-be-good')
+    -- use('ThePrimeagen/vim-be-good')
 
 	use {
 		'VonHeikemen/lsp-zero.nvim',

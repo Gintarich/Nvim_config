43a48781455561028b7ee217bc08a7d0c199f4cb
Gintarich <gintars.briedis@gmail.com>
2024-02-08 23:01:52 +0200

    More work on config

--- lua/GB/init.lua
+++ lua/GB/init.lua
@@ -12,7 +12,7 @@ autocmd('LspAttach', {
     group = GBGroup,
     callback = function(e)
         vim.cmd('set noshellslash')
-        if e.name == "omnisharp" then e.server_capabilities.semanticTokensProvider = nil end
+        -- if e.name == "omnisharp" then e.server_capabilities.semanticTokensProvider = nil end
         local opts = { buffer = e.buf}
         vim.keymap.set("n","gd",function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n","K",function() vim.lsp.buf.hover() end, opts)
@@ -20,7 +20,7 @@ autocmd('LspAttach', {
         vim.keymap.set("n","<leader>vd",function() vim.lsp.buf.open_float() end, opts)
         vim.keymap.set("n","[d",function() vim.lsp.buf.goto_next() end, opts)
         vim.keymap.set("n","]d",function() vim.lsp.buf.goto_prev() end, opts)
-        vim.keymap.set("n","<leader>vca",function() vim.lsp.buf.code_action() end, opts)
+        vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
         vim.keymap.set("n","<leader>vrr",function() vim.lsp.buf.references() end, opts)
         vim.keymap.set("n","<leader>vrn",function() vim.lsp.buf.rename() end, opts)
         vim.keymap.set("n","<C-h>",function() vim.lsp.buf.signature_help() end, opts)
